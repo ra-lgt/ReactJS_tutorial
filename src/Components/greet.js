bd02fec4e8dd3c6b750aa18a2aac47de2def0a1a
ra-lgt <raviajay9344@gmail.com>
2024-06-17 21:23:44 +0530

    intial

---
+++ src/Components/greet.js
@@ -0,0 +1,15 @@
+// Components/greet.js
+import React from 'react';
+import User from './user'
+
+function Greet() {
+  return (
+    <div>
+    <User/>
+      <h1>Welcome to our App!</h1>
+      <p>Feel free to explore.</p>
+    </div>
+  );
+}
+
+export default Greet
---
+++ src/Components/user.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function User() {
+
+	return (
+		<div>
+		<img src="../logo.png"></img>
+			
+		</div>
+	)
+}
+export default User
